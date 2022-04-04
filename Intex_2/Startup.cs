6430f7b538f7dbf087d5491c15922be423339d81
jacobgifford22 <90434868+jacobgifford22@users.noreply.github.com>
2022-04-04 12:39:36 -0600

    startup config fix

--- Intex_2/Startup.cs
+++ Intex_2/Startup.cs
@@ -1,6 +1,7 @@
 using Intex_2.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -33,6 +34,15 @@ namespace Intex_2
             });
 
             services.AddScoped<ICrashRepository, EFCrashRepository>();
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential 
+                // cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                // requires using Microsoft.AspNetCore.Http;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
