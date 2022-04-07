a1e3dcd84abb920c0737f6e4f3d88c53cf83fbe7
funsizemaddy <90282709+funsizemaddy@users.noreply.github.com>
2022-04-07 15:21:11 -0600

    first form

---
+++ Intex_2/Controllers/InferenceController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Intex_2.Models;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
+namespace Intex_2.Controllers
+{
+    public class InferenceController : Controller
+    {
+        private InferenceSession _session;
+
+        public InferenceController(InferenceSession session)
+        {
+            _session = session;
+        }
+
+        [HttpGet]
+        public IActionResult MLModel()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Score(CrashData data)
+        {
+            var result = _session.Run(new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", data.AsTensor())
+            });
+            Tensor<float> score = result.First().AsTensor<float>();
+            var prediction = new Prediction { PredictedValue = score.First() };
+            result.Dispose();
+            return View("Score", prediction);
+        }
+    }
+}
